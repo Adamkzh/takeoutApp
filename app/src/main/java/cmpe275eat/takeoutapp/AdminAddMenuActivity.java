f7efe2aea8a6dd3a3073e8fc5459e5f089a65200
YiChinHsiao <yichinhsiao@Yi-Chins-MacBook.local>
2018-05-07 16:57:07 -0700

    rename activity_admin_add_item.xml to activity_admin_add_menu.xml and update

---
+++ app/src/main/java/cmpe275eat/takeoutapp/AdminAddMenuActivity.java
@@ -0,0 +1,18 @@
+package cmpe275eat.takeoutapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yichinhsiao on 5/7/18.
+ */
+
+public class AdminAddMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_menu);
+
+    }
+}
