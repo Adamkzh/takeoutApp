e7ab08288e72deb9b1f34dacdc6f4619b71bed38
YiChinHsiao <yichinhsiao@Yi-Chins-MacBook.local>
2018-05-07 13:00:37 -0700

    add admin index

--- app/src/main/java/cmpe275eat/takeoutapp/AdminIndexActivity.java
+++ app/src/main/java/cmpe275eat/takeoutapp/AdminIndexActivity.java
@@ -1,8 +1,18 @@
 package cmpe275eat.takeoutapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by yichinhsiao on 5/7/18.
  */
 
-public class AdminIndexActivity {
+public class AdminIndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_index);
+
+    }
 }
