16226cde169aa901249e05ccac59433603c5a2c0
AdamKzh <kzh88@hotmail.com>
2018-05-10 03:54:18 -0700

    add core algorithm

--- app/src/main/java/cmpe275eat/takeoutapp/Checkout.java
+++ app/src/main/java/cmpe275eat/takeoutapp/Checkout.java
@@ -67,4 +67,10 @@ public class Checkout extends AppCompatActivity {
 
     }
 
+    public void checkOrder(){
+
+
+
+    }
+
 }
---
+++ app/src/main/java/cmpe275eat/takeoutapp/cooker/Interval.java
@@ -0,0 +1,9 @@
+package cmpe275eat.takeoutapp.cooker;
+
+
+  public class Interval {
+      int start;
+      int end;
+      Interval() { start = 0; end = 0; }
+      Interval(int s, int e) { start = s; end = e; }
+  }
