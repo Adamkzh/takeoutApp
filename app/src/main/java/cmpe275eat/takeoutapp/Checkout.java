0a3fd2fc6da3de668f27533747bae932a70e4f1d
AdamKzh <kzh88@hotmail.com>
2018-05-10 00:30:39 -0700

    add order structure

--- app/src/main/java/cmpe275eat/takeoutapp/Checkout.java
+++ app/src/main/java/cmpe275eat/takeoutapp/Checkout.java
@@ -5,12 +5,48 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import com.google.firebase.auth.*;
+import com.firebase.client.Firebase;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class Checkout extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkout);
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
     }
+
+
+    private void saveOrderToDB(){
+        String uid = "userId";
+        String pickTime = "15:50";
+        String orderStartTime = "15:30";
+
+//        Order order = new Order(uid,pickTime,orderStartTime);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
